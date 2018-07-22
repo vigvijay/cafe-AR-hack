d84eb9a1e03108b7266101da2cfe0c850cc55da9
Vignesh Vijayaraghavan <vivijaya@microsoft.com>
2018-07-22 03:07:40 -0700

    Added basic scene navigation to the app

--- ViroSample/js/SecondScene.js
+++ ViroSample/js/SecondScene.js
@@ -3,7 +3,6 @@
 import React, { Component } from 'react';
 
 import {StyleSheet} from 'react-native';
-import SecondScene from './SecondScene';
 
 import {
   ViroARScene,
@@ -19,14 +18,14 @@ import {
   ViroAnimations,
 } from 'react-viro';
 
-export default class HelloWorldSceneAR extends Component {
+export default class SecondScene extends Component {
 
   constructor() {
     super();
 
     // Set initial state here
     this.state = {
-      text : "Initializing AR..."
+     
     };
 
     // bind 'this' to functions
@@ -36,8 +35,6 @@ export default class HelloWorldSceneAR extends Component {
   render() {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-        <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} animation={{name: "rotate", run: true, loop: true}} onClick={this._pushNextScene.bind(this)}/>
         <ViroAmbientLight color={"#aaaaaa"} />
         <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]}
           position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
@@ -51,43 +48,14 @@ export default class HelloWorldSceneAR extends Component {
             scale={[.4, .4, .4]}
             type="VRX" />
         </ViroNode>
-        <ViroNode position={[2,-1,0]} dragType="FixedToWorld" onDrag={()=>{}} >
-            <Viro3DObject source={require('./res/minion/minion.obj')}
-                      resources={[require('./res/minion/minion.mtl')]}
-                      position={[-0.02, -0.4, -2]}
-                      scale={[0.1, 0.05, 0.05]}
-                      type="OBJ"
-                      onLoadStart={this._onLoadStart}
-                      onLoadEnd={this._onLoadEnd}
-                      onError={this._onError} />
-        </ViroNode>
       </ViroARScene>
     );
-  }
-
-  
-  _pushNextScene(){
-    console.log(this);
-    console.log(this.props);
-    //console.log(props);
-    console.log(SecondScene);
-    this.props.sceneNavigator.push({scene:SecondScene});
- }
-
-  _onLoadStart() {
-    console.log("OBJ loading has started"); 
-  }
-  _onLoadEnd() {
-      console.log("OBJ loading has finished");
-  }
-  _onError(event) {
-    console.log("OBJ loading failed with error: " + event.nativeEvent.error);
   }
 
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello"
+        text : ""
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
@@ -120,4 +88,4 @@ ViroAnimations.registerAnimations({
   },
 });
 
-module.exports = HelloWorldSceneAR;
+module.exports = SecondScene;
